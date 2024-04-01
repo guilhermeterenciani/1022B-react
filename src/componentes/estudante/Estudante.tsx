d974713d185ebccb2b45c0b8b5cfed0150e3db23
sh0uwzz_ <luis.ribeiro2@estudante.ifms.edu.br>
2024-04-01 10:25:47 -0400

    arrumando codigo

--- src/componentes/estudante/Estudante.tsx
+++ src/componentes/estudante/Estudante.tsx
@@ -1,20 +1,18 @@
 import './Estudante.css'
 function Estudante(props:{nome:string,imagem:string,descricao:string,githubLink:string}){
     return(
-        <main>
-            <div className="box-estudante">
-                <img src={props.imagem}></img>
+        <div className="box-estudante">
+                <img src={props.imagem} alt='Imagem Estudante'/>
                 <h3 className="nome-estudante">
                     {props.nome}
                 </h3>
                 <p className="descricao-estudante">
                     {props.descricao}
                 </p>
-                <a href='{props.githubLink}'>
+                <a href={props.githubLink}>
                     GitHub
                 </a>
             </div>
-        </main>
     )
 }
 export default Estudante
